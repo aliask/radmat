b1216dab74f3176df36ed374382ed350f7502f5b
Will Robertson <aliask@gmail.com>
2021-10-24 11:09:32 +1100

    Use black formatting, add requirements.txt

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 
 # Activate the virtual environment inside Python to allow simple execution
 import os
+
 base_dir = os.path.dirname(os.path.realpath(os.path.expanduser(__file__)))
-activate_this = os.path.join(base_dir, 'venv/bin/activate_this.py')
+activate_this = os.path.join(base_dir, "venv/bin/activate_this.py")
 exec(open(activate_this).read())
 
 import ftplib
@@ -20,96 +21,108 @@ RADAR_ID = "IDR023"
 download_root = "/tmp/radar/download/"
 ready_root = "/tmp/radar/ready/"
 
+DESTINATION_PANEL = ("127.0.0.1", 20304)
+PANEL_SIZE = (32, 16)
+
 # Avoid re-downloading every time
 FILES_TO_CACHE = 30
 
 logging.getLogger().setLevel(logging.INFO)
 
+
 def fetch_new_images(radar_id, outdir):
-  try:
-    fc = ftplib.FTP("ftp.bom.gov.au")
-    fc.login()
-    fc.cwd("/anon/gen/radar")
-  except Exception as e:
-    logging.exception(e)
-    return
+    try:
+        fc = ftplib.FTP("ftp.bom.gov.au")
+        fc.login()
+        fc.cwd("/anon/gen/radar")
+    except Exception as e:
+        logging.exception(e)
+        return
 
-  remote_files = fc.nlst()
-  for file in remote_files:
-    logging.debug("FTP file: " + file)
-    local_file = os.path.join(outdir, file)
-    if file.startswith(radar_id + ".T.") and not os.path.exists(local_file):
-      logging.info("Downloading " + file)
-      with open(local_file, 'wb') as fh:
-        fc.retrbinary('RETR ' + file, fh.write)
+    remote_files = fc.nlst()
+    for file in remote_files:
+        logging.debug("FTP file: " + file)
+        local_file = os.path.join(outdir, file)
+        if file.startswith(radar_id + ".T.") and not os.path.exists(local_file):
+            logging.info("Downloading " + file)
+            with open(local_file, "wb") as fh:
+                fc.retrbinary("RETR " + file, fh.write)
 
-  fc.close()
+    fc.close()
+
 
 def resize_image(infile, outfile):
-  im = Image.open(infile)
-  ow,oh = im.size
-  outim = im.crop(box = (0, oh*0.25, ow, oh*0.75))
-  outim = outim.resize(size = (32, 16), resample=Image.BILINEAR)
-  outim.save(outfile)
+    im = Image.open(infile)
+    ow, oh = im.size
+    outim = im.crop(box=(0, oh * 0.25, ow, oh * 0.75))
+    outim = outim.resize(size=PANEL_SIZE, resample=Image.BILINEAR)
+    outim.save(outfile)
+
 
 def resize_all_images(directory, outdir):
-  downloaded_files = os.listdir(directory)
-  for file in downloaded_files:
-    infile = os.path.join(directory, file)
-    outfile = os.path.join(outdir, file)
-    if not os.path.exists(outfile):
-      logging.debug("Resizing " + file)
-      resize_image(infile, outfile)
+    downloaded_files = os.listdir(directory)
+    for file in downloaded_files:
+        infile = os.path.join(directory, file)
+        outfile = os.path.join(outdir, file)
+        if not os.path.exists(outfile):
+            logging.debug("Resizing " + file)
+            resize_image(infile, outfile)
+
 
 def send_image(file, opened_socket):
-  logging.debug("Sending " + file)
-  frame = FramePDU()
-  frame.from_image(file)
-  pdu_bytes = frame.as_binary()
-  ret = opened_socket.sendto(pdu_bytes, ("127.0.0.1", 20304))
+    logging.debug("Sending " + file)
+    frame = FramePDU()
+    frame.from_image(file)
+    pdu_bytes = frame.as_binary()
+    ret = opened_socket.sendto(pdu_bytes, DESTINATION_PANEL)
+
 
 def send_images(files, delay):
-  opened_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  for file in files:
-    send_image(file, opened_socket=opened_socket)
-    time.sleep(delay)
+    opened_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    for file in files:
+        send_image(file, opened_socket=opened_socket)
+        time.sleep(delay)
+
 
 def update_files():
-  logging.info("Fetching new images from BOM")
-  fetch_new_images(RADAR_ID, download_root)
+    logging.info("Fetching new images from BOM")
+    fetch_new_images(RADAR_ID, download_root)
 
-  logging.info("Resizing images...")
-  resize_all_images(download_root, ready_root)
+    logging.info("Resizing images...")
+    resize_all_images(download_root, ready_root)
 
-  logging.info("Cleaning up old images...")
-  all_files = sorted(glob.glob(download_root + "/*.png"))
-  for stale_file in all_files[0:-FILES_TO_CACHE]:
-    file = os.path.basename(stale_file)
-    logging.info("Deleting " + file)
-    os.unlink(stale_file)
-    os.unlink(os.path.join(ready_root, file))
+    logging.info("Cleaning up old images...")
+    all_files = sorted(glob.glob(download_root + "/*.png"))
+    for stale_file in all_files[0:-FILES_TO_CACHE]:
+        file = os.path.basename(stale_file)
+        logging.info("Deleting " + file)
+        os.unlink(stale_file)
+        os.unlink(os.path.join(ready_root, file))
 
-  logging.info("Done. Sending images to LEDServer")
+    logging.info("Done. Sending images to LEDServer")
+
 
 def send_brightness(brightness: int):
-  logging.debug("Setting brightness to " + str(brightness))
-  cmd = CommandPDU(CommandPDU.CMD_BRIGHTNESS, brightness)
-  pdu_bytes = cmd.as_binary()
-  
-  opened_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  ret = opened_socket.sendto(pdu_bytes, ("127.0.0.1", 20304))
+    logging.debug("Setting brightness to " + str(brightness))
+    cmd = CommandPDU(CommandPDU.CMD_BRIGHTNESS, brightness)
+    pdu_bytes = cmd.as_binary()
+
+    opened_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    opened_socket.sendto(pdu_bytes, DESTINATION_PANEL)
+
 
 def main():
-  os.makedirs(download_root, exist_ok=True)
-  os.makedirs(ready_root, exist_ok=True)
-  schedule.every(5).minutes.do(update_files)
-  update_files()
-  while True:
-    schedule.run_pending()
-    send_brightness(20)
-    dir_files = sorted(glob.glob(ready_root + "/*.png"))
-    send_images(dir_files[-6:], delay=0.2)
-    time.sleep(1)
+    os.makedirs(download_root, exist_ok=True)
+    os.makedirs(ready_root, exist_ok=True)
+    schedule.every(5).minutes.do(update_files)
+    update_files()
+    while True:
+        schedule.run_pending()
+        send_brightness(20)
+        dir_files = sorted(glob.glob(ready_root + "/*.png"))
+        send_images(dir_files[-6:], delay=0.2)
+        time.sleep(1)
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- matrix_pdu.py
+++ matrix_pdu.py
@@ -3,34 +3,38 @@
 import struct
 from PIL import Image
 
-class FramePDU():
-  IDENT = 0x1234
-  height = 0
-  width = 0
-  pixeldata = b''
 
-  def from_image(self, image):
-    img = Image.open(image).convert("RGB")
-    img.load()
-    self.height = img.height
-    self.width = img.width
-    for y in range(img.height):
-      for x in range(img.width):
-        (r,g,b) = img.getpixel((x,img.height - y - 1))
-        a = 255
-        self.pixeldata += struct.pack("BBBB", r,g,b,a)
+class FramePDU:
+    IDENT = 0x1234
+    height = 0
+    width = 0
+    pixeldata = b""
 
-  def as_binary(self):
-    header = struct.pack("HHHH", self.IDENT, self.height, self.width, len(self.pixeldata))
-    return header + self.pixeldata
+    def from_image(self, image):
+        img = Image.open(image).convert("RGB")
+        img.load()
+        self.height = img.height
+        self.width = img.width
+        for y in range(img.height):
+            for x in range(img.width):
+                (r, g, b) = img.getpixel((x, img.height - y - 1))
+                a = 255
+                self.pixeldata += struct.pack("BBBB", r, g, b, a)
 
-class CommandPDU():
-  IDENT = 0x4321
-  CMD_BRIGHTNESS = 0
+    def as_binary(self):
+        header = struct.pack(
+            "HHHH", self.IDENT, self.height, self.width, len(self.pixeldata)
+        )
+        return header + self.pixeldata
 
-  def __init__(self, command, value):
-    self.command = command
-    self.value = value
 
-  def as_binary(self):
-    return struct.pack("HBB", self.IDENT, self.command, self.value)
+class CommandPDU:
+    IDENT = 0x4321
+    CMD_BRIGHTNESS = 0
+
+    def __init__(self, command, value):
+        self.command = command
+        self.value = value
+
+    def as_binary(self):
+        return struct.pack("HBB", self.IDENT, self.command, self.value)
