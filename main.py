ec8a51965c38deb47d164181c34cf046c68d0f3c
Will Robertson <aliask@gmail.com>
2023-01-08 11:18:59 +1100

    Handle panel disconnection/unavailability
    Use threading for updating images so we can keep sending frames
    Catch more exceptions
    Use more verbose logging format
    Remove obsolete set_brightness.py file

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
 
 import ftplib
+import glob
 import logging
 import os
-import glob
-from PIL import Image, UnidentifiedImageError
+import threading
 import time
-from matrix_pdu import FramePDU, CommandPDU
-import socket
+from typing import List
+
+from PIL import Image, UnidentifiedImageError
 import schedule
 
-RADAR_ID = os.environ.get("RADAR_ID", "IDR023")
-LEDSERVER_PORT = int(os.environ.get("LEDSERVER_PORT", 20304))
+from matrix_pdu import FramePDU, CommandPDU, Command, send_pdu, test_connection
 
-DESTINATION_PANEL = ("127.0.0.1", LEDSERVER_PORT)
-PANEL_SIZE = (32, 16)
+RADAR_ID = os.environ.get("RADAR_ID", "IDR023")
+
+LEDSERVER_PORT = int(os.environ.get("LEDSERVER_PORT", "20304"), base=10)
+LEDSERVER_HOST = os.environ.get("LEDSERVER_HOST", "127.0.0.1")
+DESTINATION_PANEL = (LEDSERVER_HOST, LEDSERVER_PORT)
+
+LED_WIDTH = int(os.environ.get("LED_WIDTH", "32"), base=10)
+LED_HEIGHT = int(os.environ.get("LED_HEIGHT", "16"), base=10)
+LED_BRIGHTNESS = int(os.environ.get("LED_BRIGHTNESS", "20"), base=10)
+PANEL_SIZE = (LED_WIDTH, LED_HEIGHT)
 
 # Avoid re-downloading every time
 FILES_TO_CACHE = 30
@@ -22,39 +30,49 @@ FILES_TO_CACHE = 30
 download_root = "/tmp/radar/download/"
 ready_root = "/tmp/radar/ready/"
 
-logging.getLogger().setLevel(logging.INFO)
+frame_cache_lock = threading.Lock()
+frame_cache: List[FramePDU] = []
+
+logging.basicConfig(
+    format="%(asctime)s [%(levelname)-8s] %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 
-def fetch_new_images(radar_id, outdir):
+def fetch_new_images(radar_id: str, outdir: str):
     try:
-        fc = ftplib.FTP("ftp.bom.gov.au")
-        fc.login()
-        fc.cwd("/anon/gen/radar")
-    except Exception as e:
-        logging.exception(e)
-        return
+        with ftplib.FTP("ftp.bom.gov.au") as fc:
+            fc.login()
+            fc.cwd("/anon/gen/radar")
 
-    remote_files = fc.nlst()
-    for file in remote_files:
-        logging.debug("FTP file: " + file)
-        local_file = os.path.join(outdir, file)
-        if file.startswith(radar_id + ".T.") and not os.path.exists(local_file):
-            logging.info("Downloading " + file)
-            with open(local_file, "wb") as fh:
-                fc.retrbinary("RETR " + file, fh.write)
+            remote_files = fc.nlst()
+            for file in remote_files:
+                logging.debug("FTP file: " + file)
+                local_file = os.path.join(outdir, file)
+                if file.startswith(radar_id + ".T.") and not os.path.exists(local_file):
+                    logging.info("Downloading " + file)
+                    try:
+                        with open(local_file, "wb") as fh:
+                            fc.retrbinary("RETR " + file, fh.write)
+                    except ftplib.all_errors as e:
+                        logging.error(f"Error while downloading {file} from FTP: {e}")
+                        logging.debug("Debug trace", exc_info=e)
 
-    fc.close()
+    except ftplib.all_errors as e:
+        logging.error(f"Error while connecting to FTP server (ftp.bom.gov.au): {e}")
+        logging.debug("Debug trace", exc_info=e)
 
 
-def resize_image(infile, outfile):
+def resize_image(infile: str, outfile: str):
     im = Image.open(infile)
     ow, oh = im.size
-    outim = im.crop(box=(0, oh * 0.25, ow, oh * 0.75))
+    outim = im.crop(box=(0, int(oh * 0.25, base=10), ow, int(oh * 0.75, base=10)))
     outim = outim.resize(size=PANEL_SIZE, resample=Image.BILINEAR)
     outim.save(outfile)
 
 
-def resize_all_images(directory, outdir):
+def resize_all_images(directory: str, outdir: str):
     downloaded_files = os.listdir(directory)
     for downloaded_file in downloaded_files:
         infile = os.path.join(directory, downloaded_file)
@@ -62,27 +80,30 @@ def resize_all_images(directory, outdir):
         if not os.path.exists(outfile) and downloaded_file.endswith(".png"):
             logging.debug("Resizing " + downloaded_file)
             try:
-              resize_image(infile, outfile)
+                resize_image(infile, outfile)
             except UnidentifiedImageError as e:
-              logging.warning(f"Unkown image format for file: {infile}", e)
+                logging.warning(f"Unkown image format for file: {infile}", exc_info=e)
+                os.unlink(infile)
 
 
-def send_image(file, opened_socket):
-    logging.debug("Sending " + file)
-    frame = FramePDU()
-    frame.from_image(file)
-    pdu_bytes = frame.as_binary()
-    ret = opened_socket.sendto(pdu_bytes, DESTINATION_PANEL)
-
-
-def send_images(files, delay):
-    opened_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def decode_images(files: List[str]):
+    frames: List[FramePDU] = []
     for file in files:
-        send_image(file, opened_socket=opened_socket)
+        logging.debug("Decoding " + file)
+        image = Image.open(file)
+        frames.append(FramePDU.from_image(image))
+    return frames
+
+
+def send_frames(frames: List[FramePDU], delay: float):
+    logging.debug("Sending Frames to LEDServer")
+    for frame in frames:
+        send_pdu(frame, DESTINATION_PANEL)
         time.sleep(delay)
 
 
 def update_files():
+    global frame_cache
     logging.info("Fetching new images from BOM")
     fetch_new_images(RADAR_ID, download_root)
 
@@ -97,30 +118,78 @@ def update_files():
         os.unlink(stale_file)
         os.unlink(os.path.join(ready_root, file))
 
-    logging.info("Done. Sending images to LEDServer")
+    logging.info("Converting images to Frames for panel")
+    dir_files = sorted(glob.glob(ready_root + "/*.png"))
+
+    # Pre-decode the frames, then copy to minimise lock duration
+    frames = decode_images(dir_files[-6:])
+    with frame_cache_lock:
+        frame_cache = frames.copy()
+
+    logging.info(f"Done. Sending {len(frame_cache)} images to LEDServer")
+
+
+def update_files_thread():
+    job_thread = threading.Thread(target=update_files)
+    job_thread.start()
 
 
 def send_brightness(brightness: int):
-    logging.debug("Setting brightness to " + str(brightness))
-    cmd = CommandPDU(CommandPDU.CMD_BRIGHTNESS, brightness)
-    pdu_bytes = cmd.as_binary()
-
-    opened_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    opened_socket.sendto(pdu_bytes, DESTINATION_PANEL)
+    logging.info("Setting brightness to " + str(brightness))
+    cmd = CommandPDU(Command.SetBrightness, brightness)
+    send_pdu(cmd, DESTINATION_PANEL)
 
 
 def main():
+    logging.info("Starting BOM radar streaming service with following config:")
+    logging.info(f"  - Radar ID: {RADAR_ID}")
+    logging.info(f"  - Panel Dimensions: {LED_WIDTH}x{LED_HEIGHT}")
+    logging.info(f"  - Brightness: {LED_BRIGHTNESS}")
+    logging.info(f"  - LEDServer: tcp://{LEDSERVER_HOST}:{LEDSERVER_PORT}")
+
+    # Prepare required folders
     os.makedirs(download_root, exist_ok=True)
     os.makedirs(ready_root, exist_ok=True)
-    schedule.every(5).minutes.do(update_files)
-    schedule.every(5).minutes.do(send_brightness, 20)
+
+    # Set up schedule for fetching new images
+    schedule.every(5).minutes.do(update_files_thread)
+
+    # Pre-fetch the files for first run
     update_files()
 
+    # Attempt connection to panel and set brightness
+    try:
+        send_brightness(LED_BRIGHTNESS)
+        panel_connected = True
+    except OSError as e:
+        logging.warning(f"Couldn't send to LEDServer: {e}")
+        panel_connected = False
+
+    # Main loop
     while True:
-        schedule.run_pending()
-        dir_files = sorted(glob.glob(ready_root + "/*.png"))
-        send_images(dir_files[-6:], delay=0.2)
-        time.sleep(1)
+        try:
+            if panel_connected:
+                # Update files if it's time
+                schedule.run_pending()
+
+                # Create a copy of the frame cache in to minimise lock duration
+                with frame_cache_lock:
+                    frames = frame_cache.copy()
+
+                # Send a cycle of the radar images
+                send_frames(frames=frames, delay=0.2)
+
+                # Pause between radar cycles
+                time.sleep(1)
+            else:
+                time.sleep(60)
+                logging.info(f"Retrying connection to LEDServer at tcp://{LEDSERVER_HOST}:{LEDSERVER_PORT}...")
+                panel_connected = test_connection(DESTINATION_PANEL)
+                if panel_connected:
+                    send_brightness(LED_BRIGHTNESS)
+        except OSError as e:
+            panel_connected = False
+            logging.warning(f"Couldn't send to LEDServer: {e}")
 
 
 if __name__ == "__main__":
--- matrix_pdu.py
+++ matrix_pdu.py
@@ -1,40 +1,84 @@
 #!/usr/bin/env python3
 
+from abc import ABCMeta, abstractmethod
+from enum import Enum
+import logging
+import socket
 import struct
+
 from PIL import Image
 
 
-class FramePDU:
-    IDENT = 0x1234
-    height = 0
-    width = 0
-    pixeldata = b""
+class NetworkPDU(object, metaclass=ABCMeta):
+    IDENT = 0x0000
 
-    def from_image(self, image):
-        img = Image.open(image).convert("RGB")
+    @abstractmethod
+    def as_binary(self) -> bytes:
+        pass
+
+
+class FramePDU(NetworkPDU):
+    pixels: bytes
+    height: int
+    width: int
+    IDENT = 0x1234
+
+    def __init__(self, height: int, width: int, pixels: bytes):
+        self.height = height
+        self.width = width
+        self.pixels = pixels
+
+    @classmethod
+    def from_image(cls, image: Image.Image):
+        img = image.convert("RGB")
         img.load()
-        self.height = img.height
-        self.width = img.width
+        pixels = b""
         for y in range(img.height):
             for x in range(img.width):
                 (r, g, b) = img.getpixel((x, img.height - y - 1))
                 a = 255
-                self.pixeldata += struct.pack("BBBB", r, g, b, a)
+                pixels += struct.pack("BBBB", r, g, b, a)
+        return cls(height=img.height, width=img.width, pixels=pixels)
 
     def as_binary(self):
         header = struct.pack(
-            "HHHH", self.IDENT, self.height, self.width, len(self.pixeldata)
+            "HHHH", self.IDENT, self.height, self.width, len(self.pixels)
         )
-        return header + self.pixeldata
+        return header + self.pixels
 
 
-class CommandPDU:
+class Command(Enum):
+    SetBrightness = 0
+    SetPriority = 1
+
+
+class CommandPDU(NetworkPDU):
+    command: Command
+    value: int
     IDENT = 0x4321
-    CMD_BRIGHTNESS = 0
 
     def __init__(self, command, value):
         self.command = command
         self.value = value
 
     def as_binary(self):
-        return struct.pack("HBB", self.IDENT, self.command, self.value)
+        return struct.pack("HBB", self.IDENT, self.command.value, self.value)
+
+
+def send_pdu(frame: NetworkPDU, destination: tuple):
+    pdu_bytes = frame.as_binary()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(destination)
+        s.sendall(pdu_bytes)
+
+
+def test_connection(destination: tuple) -> bool:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect(destination)
+        except OSError as e:
+            logging.info(f"Connection failed: {e}")
+            return False
+    logging.info("Connected!")
+    return True
+    
